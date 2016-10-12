328f42bd554181690c0b5382bee11c94f74cf3a4
Liz Peacock <liz.peacock11@gmail.com>
2016-10-12 17:15:04 -0500

    created a blade for editing posts

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+
+    <title>Create a Post</title>
+
+@stop
+
+@section('content')
+    
+    <form class="form" method="POST" action="{{ action('PostsController@update', $post->id) }}">
+        {!! csrf_field() !!}
+        {!! method_field('PUT') !!}
+        Title: <input class="form-control" type="text" name="title" value="{{ (old('title') == null) ? $post->title : old('title') }}">
+        Content: <textarea class="form-control" name="content" rows="5" cols="40">{{ (old('content') == null) ? $post->content : old('content') }}</textarea>
+        Link to URL: <input class="form-control" type="text" name="url" value="{{ (old('url') == null) ? $post->url : old('url') }}">
+        <input class="btn-success btn" type="submit">
+    </form>
+
+@stop
