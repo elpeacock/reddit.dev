cc778dde3d624534cd51287afe0a30b301b5aeea
Liz Peacock <liz.peacock11@gmail.com>
2016-10-13 17:11:23 -0500

    updated post edit page to show error messages under inputs

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,11 +9,31 @@
 @section('content')
     
     <form class="form" method="POST" action="{{ action('PostsController@update', $post->id) }}">
+
         {!! csrf_field() !!}
         {!! method_field('PUT') !!}
+
         Title: <input class="form-control" type="text" name="title" value="{{ (old('title') == null) ? $post->title : old('title') }}">
+
+        @if ($errors->has('title'))
+            <br>
+            <div class="alert alert-danger">{{ $errors->first('title') }}</div>
+        @endif
+
         Content: <textarea class="form-control" name="content" rows="5" cols="40">{{ (old('content') == null) ? $post->content : old('content') }}</textarea>
+
+        @if ($errors->has('content'))
+            <br>
+            <div class="alert alert-danger">{{ $errors->first('content') }}</div>
+        @endif
+
         Link to URL: <input class="form-control" type="text" name="url" value="{{ (old('url') == null) ? $post->url : old('url') }}">
+
+        @if ($errors->has('url'))
+            <br>
+            <div class="alert alert-danger">{{ $errors->first('url') }}</div>
+        @endif
+
         <input class="btn-success btn" type="submit">
     </form>
 
