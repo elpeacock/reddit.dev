00420049703206726a458f93297c2236ce748024
Liz Peacock <liz.peacock11@gmail.com>
2016-10-18 12:15:31 -0500

    updated Post model to extend basemodel and added search posts function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel;
 
-class Post extends Model
+class Post extends BaseModel
 {
     protected $table = 'posts';
 
-    
+    public static $rules = [
+    	'title' => 'required | max:100',
+        'content' => 'required',
+        'url' => 'required',
+    ];
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User', 'created_by', 'id');
+    }
+
+    public static function searchPosts($searchTerm)
+    {
+    	return Post::where('title', 'LIKE', '%' . $searchTerm . '%')->get();
+    }
+
+
 }
