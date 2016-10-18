256a6339b05343ac5d6a47b72caa397bb8538339
Liz Peacock <liz.peacock11@gmail.com>
2016-10-18 12:13:07 -0500

    added destroy functionality to posts controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Post;
+use App\Models\BaseModel;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -15,14 +16,46 @@ class PostsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::paginate(2);
-        $data = ['posts' => $posts];
         
-        return view('posts.index')->with($data);
+        $posts = Post::with('user')->paginate(10);
+        // for search bar functionality
+        if (isset($request['search'])) {
+
+            $search = Post::searchPosts($searchTerm);
+
+            if ($search->get()) {
+
+                $posts = $search->paginate(10);
+            
+            } else {
+            
+                $posts = Post::with('user')->paginate(10);
+
+            }
+        
+        }
+
+        $data = ['posts' => $posts];
+        return view('posts.index')->with($data);  
 
     }
+        // next 2 lines should go in usercontroller
+        // $user = User::find(2);
+        // dd(user->posts) 
+
+        // query building
+        // all posts that have 'lorem' in their title
+        // SELECT * FROM POSTS
+        // WHERE title LIKE '%lorem%'
+        // $posts = Post::where('title', 'NOT LIKE', '%lorem%')
+        //             // method chaining = adding addtl wheres or orderby etc...
+        //             ->where('content', 'NOT LIKE', '%lorem%')->take(8)->get();
+        //             // DONT FORGET THE get()!!!!
+        //             // take() = limit
+        //             // skip() = offset
+        // dd($posts);
 
     /**
      * Show the form for creating a new resource.
@@ -43,11 +76,6 @@ class PostsController extends Controller
     public function store(Request $request)
     {
         
-        $rules = array(
-            'title' => 'required | max:100',
-            'content' => 'required',
-            // 'url' => 'required',
-            );
 
         $request->session()->flash('ERROR_MESSAGE', 'Post was not saved. See messages below input fields and try again.');
         $this->validate($request, $rules);
@@ -136,6 +164,20 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        return 'delete the post with id # $id';
+        $post = Post::find($id);
+        
+        if (!$post) {
+            
+            abort(404);
+            $request->session()->flash('ERROR_MESSAGE', 'Post not deleted successfully. Try Again.');
+
+        }
+
+        $request->session()->forget('ERROR_MESSAGE');
+        $post->delete();
+
+        $request->session()->flash('SUCCESS_MESSAGE', 'Post was deleted successfully.');
+        
+        return redirect()->action('PostsController@index');
     }
 }
